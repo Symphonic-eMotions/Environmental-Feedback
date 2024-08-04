5dd1fcd6497178dd0c557361b9fc69e15a0dfd7e
Symphonic eMotions <info@symphonic-emotions.nl>
2024-08-04 09:16:57 +0200

    Initial Commit

---
+++ Environmental Feedback/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Environmental Feedback
+//
+//  Created by Frans-Jan Wind on 04/08/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Environmental Feedback/Environmental_FeedbackApp.swift
@@ -0,0 +1,17 @@
+//
+//  Environmental_FeedbackApp.swift
+//  Environmental Feedback
+//
+//  Created by Frans-Jan Wind on 04/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct Environmental_FeedbackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
